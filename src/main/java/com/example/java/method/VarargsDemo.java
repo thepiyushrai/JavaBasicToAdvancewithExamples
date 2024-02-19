e85775033cb0fd59dbff06d0662ac11ad0d742ac
piyushrai <thepiyushrai@gmail.com>
2024-02-20 02:52:15 +0530

    Deeep dive with example about Java Oops

---
+++ src/main/java/com/example/java/method/VarargsDemo.java
@@ -0,0 +1,38 @@
+package com.example.java.method;
+
+/**
+ * @author Piyush Rai
+ * @date 20/02/24
+ * @time 2:47 am
+ */
+public class VarargsDemo {
+    public static void main(String... args) {
+        System.out.println(sum(1, 2, 3, 4, 5));
+        System.out.println(sum(5));
+    }
+
+    private static int sum(int i1, int i2) {
+        return i1 + i2;
+    }
+
+    private static int sum(int i1, int i2, int i3) {
+        return i1 + i2 + i3;
+    }
+
+    private static int sum(int... ints) {
+        int sum = 0;
+        for (int i : ints) {
+            sum += i;
+        }
+        return sum;
+    }
+
+//	private static void demoVarargsIncorrect(int... i, String s) {
+//
+//	}
+
+    private static void demoVarargs(String s, int... i) {
+
+    }
+
+}
